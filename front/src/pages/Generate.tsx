171b348a801b1f9e05240a300239bf292b95c63e
nonrita <2220025@ecc.ac.jp>
2024-02-11 13:50:57 +0900

    add:ブックマーク一覧と画像生成一覧画面のバリエーションを追加しました

--- front/src/pages/Generate.tsx
+++ front/src/pages/Generate.tsx
@@ -1,14 +1,33 @@
 import { IoBookmark } from "react-icons/io5";
-import TestImg from "../images/scroll-img.jpeg";
 import { useState } from "react"; // Import useState hook
 
-const map = {
-  0: {
+import TestImg from "../images/scroll-img.jpeg";
+import ScrollImg1 from "../images/demo-img1.jpeg";
+import ScrollImg2 from "../images/demo-img2.jpeg";
+import ScrollImg3 from "../images/demo-img3.jpeg";
+import ScrollImg4 from "../images/demo-img4.jpeg";
+import ScrollImg5 from "../images/demo-img5.jpeg";
+
+const map = [
+  {
+    urls: [ScrollImg2, ScrollImg1],
     firstWord: "ツナ",
+    memo: "memo1",
     secondWord: "ガリ",
-    image: [TestImg, TestImg],
   },
-};
+  {
+    urls: [ScrollImg5, TestImg],
+    firstWord: "ツナ",
+    memo: "memo1",
+    secondWord: "ガリ",
+  },
+  {
+    urls: [ScrollImg3, ScrollImg1, TestImg],
+    firstWord: "ツナ",
+    memo: "memo1",
+    secondWord: "ガリ",
+  },
+];
 
 const Generate = () => {
   const [generatedContent, setGeneratedContent] = useState<JSX.Element | undefined>(undefined);
@@ -20,7 +39,7 @@ const Generate = () => {
         {Object.entries(map).map(([key, value]) => (
           <div key={key} className="w-fit h-fit p-4 bg-indigo-950 rounded-xl flex flex-col lg:flex-row gap-2">
             <div className="w-full lg:w-1/3 p-2 rounded-xl border-4 border-white grid grid-cols-2 grid-rows-2 gap-2 aspect-square">
-              {value.image.map((img, index) => (
+              {value.urls.map((img, index) => (
                 <img key={index} src={img} alt="" className="col-span-1 row-span-1" />
               ))}
             </div>
@@ -55,7 +74,33 @@ const Generate = () => {
         <button onClick={randomGenerate} className="w-fit px-6 py-4 my-2 rounded-lg text-lg lg:text-3xl font-bold text-indigo-950 bg-white">
           ランダム生成
         </button>
-        {generatedContent}
+        <div className=" min-h-96 p-4 m-2 mt-8 rounded-xl bg-indigo-900 text-sm lg:text-xl flex flex-col gap-4">
+          {map.map((value, index) => (
+            <div className=" w-fit h-fit p-4 bg-indigo-950 rounded-xl flex flex-col lg:flex-row gap-2">
+              <div className=" w-full lg:w-1/3 p-2 rounded-xl border-4 border-white grid grid-cols-2 grid-rows-2 gap-2 aspect-square">
+                {value.urls.map((url, index) => (
+                  <img key={index} src={url} alt="" className="col-span-1 row-span-1" />
+                ))}
+              </div>
+              <div className=" w-full lg:w-2/3 col-span-5 aspect-square lg:aspect-[2/1] border-4 rounded-xl border-white  grid lg:grid-rows-7">
+                <div className=" row-span-1 text-white flex flex-row items-center justify-around">
+                  <div className=" flex flex-col lg:flex-row lg:gap-8">
+                    <p className=" text-base lg:text-xl font-bold">
+                      単語１ : <span className="text-sm lg:text-lg">{value.firstWord}</span>
+                    </p>
+                    <p className=" text-base lg:text-xl font-bold">
+                      単語２ : <span className="text-sm lg:text-lg">{value.secondWord}</span>
+                    </p>
+                  </div>
+                  <div className="">
+                    <IoBookmark className={` size-8 ${false ? "text-red-500" : "text-gray-400"} `} />
+                  </div>
+                </div>
+                <textarea name="" id="" className=" p-2 row-span-6" placeholder="メモしてね！"></textarea>
+              </div>
+            </div>
+          ))}
+        </div>
       </section>
     </main>
   );
