0956820a60fde64c39c63ce57c9b532a7e8a3aeb
Fuuma <masa81hiro69@gmail.com>
2024-02-11 10:36:44 +0900

    feat: メールアドレスを削除

--- front/src/pages/Profile.tsx
+++ front/src/pages/Profile.tsx
@@ -20,10 +20,6 @@ const Profile = () => {
       </h1>
 
       <section className='w-11/12 lg:w-4/5 p-4 mt-2 mx-auto flex flex-col items-start gap-12'>
-        <p className='text-base lg:text-2xl font-bold text-white'>
-          メールアドレス : test@gmail.com
-        </p>
-
         <div className='w-full py-8 border-4 border-white rounded-lg flex items-center justify-around'>
           <button
             className='px-4 py-2 bg-white text-sm lg:text-2xl rounded-lg font-bold text-indigo-950'
