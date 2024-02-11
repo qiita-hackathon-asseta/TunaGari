256f49ab7586762c50414d74d9ef50857391b237
Fuuma <masa81hiro69@gmail.com>
2024-02-11 09:30:02 +0900

    feat: ブックマーク表示用のやつを追加

---
+++ back/functions/src/get/getAllIdeas.ts
@@ -0,0 +1,35 @@
+import { https } from "firebase-functions";
+
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+export const getAllIdeas = https.onRequest(async (req, res) => {
+  try {
+    const userId = req.query.userId;
+
+    if (!userId) {
+      throw new Error("User ID is required.");
+    }
+
+    // Get a reference to the user's ideas collection
+    const ideasCollection = admin
+      .firestore()
+      .collection(`users/${userId}/ideas`);
+
+    // Get all ideas for the specified user
+    const snapshot = await ideasCollection.get();
+
+    const ideas: any[] = [];
+    snapshot.forEach((doc: any) => {
+      const idea = doc.data();
+      ideas.push(idea);
+    });
+
+    res.json(ideas);
+    res.status(200);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
